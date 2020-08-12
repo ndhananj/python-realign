019b67ddfef976156dd84a2952a162fcdba6bcce
Nithin Dhananjayan <Dhananjayan_Nithin1@Allergan.com>
2020-08-12 00:55:45 -0700

    Rename primary_mode to mode

--- modes.py
+++ modes.py
@@ -36,11 +36,11 @@ def get_xvg_stats(xvgfile,fitfile=None,unbias=False):
     mean1, mean2, cov, s, u, v = calc_coord_stats(coords,coords,unbias=unbias)
     return mean1, mean2, cov, s, u, v
 
-def shift_by_mode(df,primary_mode,indeces,mul):
+def shift_by_mode(df,mode,indeces,mul):
     stat_items=['x_coord', 'y_coord', 'z_coord']
     to_change = df.iloc[match_col_in_int_list(df,'atom_number',indeces)]
     coords = to_change[stat_items]
-    coords += primary_mode*float(mul)
+    coords += mode*float(mul)
     to_ret = to_change.copy()
     to_ret[stat_items] = coords
     return to_ret
@@ -49,17 +49,22 @@ def shift_by_mode(df,primary_mode,indeces,mul):
 def get_atom_participation_from_eigenvector(S):
     return np.sum(S**2,axis=1)
 
-# normalize and get scales values
+# normalize and get scaled values
 def get_coloring(P):
     m = np.max(P)
     return np.cbrt(P/m)
+
+# Will retunr in KJ/mol/Angtrom^2 assuming D is in Angtrom^2
+def spring_constants_from_variances(D,T=293.1):
+    R=8.3145
+    return R*T/D
 
 def modes(xvgfile,ndxfile,pdbfile,mode,newpdbfile,mul,fit_using_pdb=True):
     fitfile = pdbfile if fit_using_pdb else None
     mean1, mean2, cov, s, u, v = get_xvg_stats(xvgfile,fitfile=fitfile)
     shift_shape = (int(u.shape[1]/3),3)
-    primary_mode = u[:,int(mode)].reshape(shift_shape)
-    print("u[:,",mode,"] =",primary_mode)
+    mode = u[:,int(mode)].reshape(shift_shape)
+    print("u[:,",mode,"] =",mode)
     ndx=read_ndx(ndxfile)
     print(ndx['C-alpha'])
     ppdb=PandasPdb()
@@ -67,7 +72,7 @@ def modes(xvgfile,ndxfile,pdbfile,mode,newpdbfile,mul,fit_using_pdb=True):
     ts=np.linspace(-float(0),float(400),num=401)
     muls=float(mul)*np.cos(np.pi*ts/200)
     for i in range(len(muls)):
-        new_df = shift_by_mode(ppdb.df['ATOM'],primary_mode,ndx['C-alpha'],muls[i])
+        new_df = shift_by_mode(ppdb.df['ATOM'],mode,ndx['C-alpha'],muls[i])
         mode_pdb = PandasPdb()
         mode_pdb.df['ATOM'] = new_df
         mode_pdb.to_pdb(path=newpdbfile+str(i)+'.pdb',\
