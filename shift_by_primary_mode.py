888111c7ce7555f97e7cf909ed6d4bcc26aaca70
Nithin Dhananjayan <Dhananjayan_Nithin1@Allergan.com>
2020-08-18 22:31:56 -0700

    Change the inputs to shift_by_primarey_mode.py

--- shift_by_primary_mode.py
+++ shift_by_primary_mode.py
@@ -3,4 +3,10 @@ from modes import *
 import sys
 
 if __name__ == '__main__':
-    modes(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    mn = [i for i in range(int(sys.argv[4]),int(sys.argv[5])+1)]
+    if len(sys.argv)>8:
+        modes(sys.argv[1],sys.argv[2],sys.argv[3], mn,\
+           sys.argv[6],sys.argv[7],ndx_name=sys.argv[8])
+    else:
+        modes(sys.argv[1],sys.argv[2],sys.argv[3],mn,\
+           sys.argv[6],sys.argv[7])
