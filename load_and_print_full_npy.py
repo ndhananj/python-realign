cd9a649fcd0b19ad8755949ede787cb33ba7714d
Nithin Dhananjayan <Dhananjayan_Nithin1@Allergan.com>
2020-09-08 23:46:06 -0700

    Add stats to load_and_print_full_npy.py

--- load_and_print_full_npy.py
+++ load_and_print_full_npy.py
@@ -4,4 +4,7 @@ from modes import *
 if __name__ == '__main__':
     m=load_matrix(sys.argv[1])
     np.set_printoptions(threshold=sys.maxsize)
-    print(m)
+    print("data:",m)
+    print("shape:",m.shape)
+    print("max:",np.max(m))
+    print("argmax:",np.argmax(m))
